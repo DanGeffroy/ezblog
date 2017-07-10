d0b245576997de289a8eef29b24ed1d3f3b9ebc9
Dan <dan.geffroy@capgemini.com>
2017-07-10 14:50:07 +0200

    correcting for production

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import {  BrowserRouter as Router,  Route,  Link} from 'react-router-dom';
 const Home = () => (
   <div>
   <div className="title-hide homepage">
-    <img src={"../logo.png"} alt="logo of the app" className="app-logo"></img>
+    <img src={"./logo.png"} alt="logo of the app" className="app-logo"></img>
     <div className="spacer"></div>
      <Link className="btn-white" to={`/ezblog/articles/`}>
        see articles here
